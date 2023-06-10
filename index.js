fe7f60b4d0944b6c2f3f82b39c1cf4739d63a484
Omar Richards <omarrichards01@gmail.com>
2023-06-10 19:24:46 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+if (someNumber <= 400){
+  return "This one is on me!"
+} else if (someNumber > 400 && someNumber < 2000) {
+  return "That will be twenty bucks."
+} else if (someNumber > 2000 && someNumber < 2500) {
+  return "I will gladly take your thirty bucks."
+} else if (someNumber >= 2500 ) {
+  return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return city == "NYC" ? "Ok, sounds good." : "No go."
+}
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  
+  switch (tip) {
+    case "generous":
+      return 'Thank you so much.'
+       break;
+    case "not as generous":
+      return 'Thank you.'
+      break;
+    
+    default:
+       return 'Bye.'
+  }
 }
